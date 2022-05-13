40a49f8cb6dcf90662bba53b2ec0ec3b9b7559f0
jkwon <kwonejk@gmail.com>
2022-05-14 02:58:23 +0800

    movie app part one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const  [loading, setLoading] = useState(true); 
+  const [movies, setMovies] = useState([]); 
+
+  //async와 await로 써주기 위해 따로 함수로 빼둠. 
+  const getMovies = async() => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`); 
+    const json = await response.json(); 
+    setMovies(json.data.movies);
+    setLoading(false); 
+  }; 
+
+  useEffect(()=> {
+      getMovies(); 
+  }, []); 
+
   return (
-    <div className="App">
-      Hello React
-    </div>
+      <div>{loading? (
+        <h1>Loading...</h1> ) :
+         <div>
+           {movies.map( (movie)=> 
+                <div key={movie.id}>
+                  <img src={movie.medium_cover_image} />
+                  <h2>{movie.title}</h2>
+                  <p>{movie.summary}</p>
+                  <ul> {movie.genres &&  
+                   movie.genres.map((g)=> <li key={g}>{g}</li>)}
+                  } </ul>
+                  </div>
+                  )}
+           </div> 
+          }
+        </div>
   );
 }
 
